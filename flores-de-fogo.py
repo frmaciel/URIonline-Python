a6696dfd80a6ffe9aaf0397e12c5c716a6601c8f
frmaciel <39967633+frmaciel@users.noreply.github.com>
2020-10-05 22:12:57 -0300

    Create flores-de-fogo.py

---
+++ flores-de-fogo.py
@@ -0,0 +1,27 @@
+while True:
+    try:
+        r1, x1, y1, r2, x2, y2 = [int(x) for x in input().split()]
+        dist_orig1_orig2 = (((x1-x2)**2)+((y1-y2)**2))**(1/2)
+        cond_relacao_circulo = ""
+        
+        if r2 > r1:
+            print('Morto')
+        else:        
+            if(dist_orig1_orig2 > r1 + r2):
+                cond_relacao_circulo = "externas"
+            if(dist_orig1_orig2 == r1 + r2):
+                cond_relacao_circulo = "tang_externa"
+            if(dist_orig1_orig2 == r1 - r2):
+                cond_relacao_circulo = "tang_interna"
+            if((dist_orig1_orig2 > r1 - r2) and (dist_orig1_orig2 < r1 + r2)):
+                cond_relacao_circulo = "secantes"
+            if(dist_orig1_orig2 < r1 - r2):
+                cond_relacao_circulo = "internas"
+
+            if ((cond_relacao_circulo == "tang_interna") or (cond_relacao_circulo == "internas")):
+                print('Rico')
+            else:
+                print('Morto')
+
+    except EOFError:
+        break
