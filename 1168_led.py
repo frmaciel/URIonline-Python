ff2585d1b09015581dd700968c7335584ec49e79
frmaciel <frmaciel@gmail.com>
2020-10-07 00:05:56 -0300

    Update 1168_led.py

--- 1168_led.py
+++ 1168_led.py
@@ -1,8 +1,24 @@
 while True:
     try:
         n = int(input())
-        for x in range (0, n):
+        for x in range (n):
             num = str(input())
-            print(len(num))
+            led = 0
+            z = len(num)
+            for y in range(z):
+                if((num[y] == 6) or (num[y] == 9) or (num[y] == 0)):
+                    led = led + 6
+                if((num[y] == 2) or (num[y] == 3) or (num[y] == 5)):
+                    led = led + 5
+                if(num[y] == 1):
+                    led = led + 2
+                if(num[y] == 7):
+                    led = led + 3
+                if(num[y] == 4):
+                    led = led + 4
+                if(num[y] == 8):
+                    led = led + 7
+            
+            print('{:d} leds'.format(led))
     except EOFError:
         break
