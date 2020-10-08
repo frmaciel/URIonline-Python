e9752e55f2fba06a781a69669087dc06617a2e89
frmaciel <frmaciel@gmail.com>
2020-10-08 00:31:11 -0300

    Update 1168_led.py

--- 1168_led.py
+++ 1168_led.py
@@ -1,24 +1,26 @@
 while True:
     try:
         n = int(input())
-        for x in range (n):
-            num = str(input())
-            led = 0
-            z = len(num)
-            for y in range(z):
-                if((num[y] == 6) or (num[y] == 9) or (num[y] == 0)):
-                    led = led + 6
-                if((num[y] == 2) or (num[y] == 3) or (num[y] == 5)):
-                    led = led + 5
-                if(num[y] == 1):
-                    led = led + 2
-                if(num[y] == 7):
-                    led = led + 3
-                if(num[y] == 4):
-                    led = led + 4
-                if(num[y] == 8):
-                    led = led + 7
-            
-            print('{:d} leds'.format(led))
+        if((n >= 1) and (n <= 1000)):
+            for item in range(n):
+                num = int(input ())
+                if((num >= 1) and (num <= 10**100)):
+                    led = 0
+                    num = str(num)
+                    for y in num:
+                        if((y == '6') or (y == '9') or (y == '0')):
+                            led = led + 6
+                        if((y == '2') or (y == '3') or (y == '5')):
+                            led = led + 5
+                        if(y == '1'):
+                            led = led + 2
+                        if(y == '7'):
+                            led = led + 3
+                        if(y == '4'):
+                            led = led + 4
+                        if(y == '8'):
+                            led = led + 7
+                    
+                    print('{:d} leds'.format(led))
     except EOFError:
         break
